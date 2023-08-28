4f21c9e044712ab75e616786aba7307fa460eeab
darkohedgehog <darkohedgehog>
2023-08-28 15:55:21 +0000

    First Commit from Locofy Builder

---
+++ components/sell-your-home-container.js
@@ -0,0 +1,41 @@
+import { useMemo } from "react";
+
+const SellYourHomeContainer = ({
+  homeSaleRentId,
+  homeSaleRentText,
+  propWidth,
+}) => {
+  const sellYourHomeStyle = useMemo(() => {
+    return {
+      width: propWidth,
+    };
+  }, [propWidth]);
+
+  return (
+    <div
+      className="rounded bg-gray-white shadow-[0px_24px_68px_rgba(59,_77,_129,_0.08)] flex flex-col p-[22px] box-border items-start justify-start min-w-[300px] max-w-[340px] text-left text-base text-gray-700 font-body-large-400"
+      style={sellYourHomeStyle}
+    >
+      <div className="w-[268px] h-[254px] flex flex-col items-start justify-start gap-[24px]">
+        <img
+          className="relative w-[78px] h-[78px] overflow-hidden shrink-0"
+          alt=""
+          src={homeSaleRentId}
+        />
+        <div className="flex flex-col items-start justify-start gap-[24px]">
+          <div className="relative text-5xl leading-[32px] font-semibold text-center">
+            {homeSaleRentText}
+          </div>
+          <div className="relative leading-[24px] text-lightslategray flex items-end w-[268px]">
+            We do a free evaluation to be sure you want to start selling.
+          </div>
+          <div className="relative leading-[24px] text-primary-500">
+            Read more
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SellYourHomeContainer;
